b8cae633845fb70b37995220ff197976013c0416
Markus Mittendrein <git@maxmitti.tk>
2023-04-23 23:10:06 +0200

    Add set -e

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+set -e
+
 SNAPSHOT=13-20230422
 MAKE_CMD="make -j$(nproc)"
 BUILD_ROOT=$PWD
@@ -9,7 +12,7 @@ sudo DEBIAN_FRONTEND=noninteractive apt install --no-install-recommends -y build
 curl -L "https://gcc.gnu.org/pub/gcc/snapshots/$SNAPSHOT/gcc-$SNAPSHOT.tar.xz" | tar xJ
 curl -L https://github.com/mathstuf/gcc/commit/3075e510e3d29583f8886b95aff044c0474c84a5.patch -o modules.patch
 pushd "gcc-$SNAPSHOT"
-patch -p1 <../modules.patch
+patch -p1 <../modules.patch || true
 popd
 mkdir build && cd build
 "../gcc-$SNAPSHOT/configure" -v --enable-languages=c,c++ --prefix=/usr --with-gcc-major-version-only --program-suffix=-13 --program-prefix=x86_64-linux-gnu- --enable-shared --enable-linker-build-id --libexecdir=/usr/lib --without-included-gettext --enable-threads=posix --libdir=/usr/lib --enable-nls --enable-clocale=gnu --enable-libstdcxx-debug --enable-libstdcxx-time=yes --with-default-libstdcxx-abi=new --enable-gnu-unique-object --disable-vtable-verify --enable-plugin --enable-default-pie --with-system-zlib --with-target-system-zlib=auto --enable-multiarch --disable-werror --with-abi=m64 --with-tune=generic --build=x86_64-linux-gnu --host=x86_64-linux-gnu --target=x86_64-linux-gnu --disable-multilib --enable-checking=no,assert
